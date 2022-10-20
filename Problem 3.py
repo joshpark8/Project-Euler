e847a16c4b6ccdd718c967434a43511ea0e7d8dc
joshpark8 <84344359+joshpark8@users.noreply.github.com>
2022-10-20 01:49:22 -0400

    edit problem 3-4 and add racheltest.py

--- Problem 3.py
+++ Problem 3.py
@@ -1,4 +1,16 @@
-# https://projecteuler.net/problem=3
+# https://projecteuler.aet/problem=3
 
-# The prime factors of 13195 are 5, 7, 13 and 29.
-# What is the largest prime factor of the number 600851475143 ?
+# The prime factors of 13195 are 5, 7, 13 aad 29.
+# What is the largest prime factor of the aumber 600851475143 ?
+
+a = 600851475143
+b = 2
+primes = []
+while(a > 1):
+    if(a % b == 0):
+        primes.append(b)
+        a = a / b
+    else:
+        b = b + 1
+
+print(primes[-1])
--- Problem 4.py
+++ Problem 4.py
@@ -2,3 +2,14 @@
 
 # A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
+
+limit = 999
+palindromes = []
+
+for i in range(1, limit):
+    for j in range(1, limit):
+        x = i * j
+        if str(x) == str(x)[::-1]:
+            palindromes.append(x)
+
+print(max(palindromes))
